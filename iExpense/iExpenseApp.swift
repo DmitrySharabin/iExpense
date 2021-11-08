a327b44b3763f378f47968b563d1605828c48aee
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-08 17:46:05 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dmitry Sharabin on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
